e152ef1b057664bb3b36f19f014ff3a40b5b453b
Sebastien Bacher <sebastien.bacher@canonical.com>
2025-07-18 11:31:32 +0200

    Include unit tests

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,18 +1,218 @@
-# Copyright 2025 Ubuntu
+# Copyright 2025 Canonical
 # See LICENSE file for licensing details.
-#
-# Learn more about testing at: https://juju.is/docs/sdk/testing
 
+"""Unit tests for the charm.
 
-from ops import testing
+These tests only cover those methods that do not require internet access,
+and do not attempt to manipulate the underlying machine.
+"""
+
+from subprocess import CalledProcessError
+from unittest.mock import patch
+
+import pytest
+from charms.operator_libs_linux.v0.apt import PackageError, PackageNotFoundError
+from git import GitCommandError
+from ops.testing import (
+    ActiveStatus,
+    BlockedStatus,
+    Context,
+    MaintenanceStatus,
+    Secret,
+    State,
+)
+from requests.exceptions import RequestException
 
 from charm import UbuntuLangpacksCharm
 
 
-def test_start():
-    # Arrange:
-    ctx = testing.Context(UbuntuLangpacksCharm)
-    # Act:
-    state_out = ctx.run(ctx.on.start(), testing.State())
-    # Assert:
-    assert state_out.unit_status == testing.ActiveStatus()
+@pytest.fixture
+def ctx():
+    return Context(UbuntuLangpacksCharm)
+
+
+@pytest.fixture
+def base_state(ctx):
+    return State(leader=True)
+
+
+@patch("charm.Langpacks.install")
+def test_install_success(install_mock, ctx, base_state):
+    install_mock.return_value = True
+    out = ctx.run(ctx.on.install(), base_state)
+    assert out.unit_status == ActiveStatus("")
+    assert install_mock.called
+
+
+@patch("charm.Langpacks.install")
+@pytest.mark.parametrize(
+    "exception",
+    [
+        PackageError,
+        PackageNotFoundError,
+        GitCommandError(command="git clone", status=128),
+        CalledProcessError(1, "foo"),
+    ],
+)
+def test_install_failure(mock, exception, ctx, base_state):
+    mock.side_effect = exception
+    out = ctx.run(ctx.on.install(), base_state)
+    assert out.unit_status == BlockedStatus(
+        "Failed to set up the environment. Check `juju debug-log` for details."
+    )
+
+
+@patch("charm.Langpacks.install")
+def test_upgrade_success(install_mock, ctx, base_state):
+    install_mock.return_value = True
+    out = ctx.run(ctx.on.upgrade_charm(), base_state)
+    assert out.unit_status == ActiveStatus("")
+    assert install_mock.called
+
+
+@patch("charm.Langpacks.install")
+@pytest.mark.parametrize(
+    "exception",
+    [
+        PackageError,
+        PackageNotFoundError,
+        GitCommandError(command="git clone", status=128),
+        CalledProcessError(1, "foo"),
+    ],
+)
+def test_upgrade_failure(mock, exception, ctx, base_state):
+    mock.side_effect = exception
+    out = ctx.run(ctx.on.upgrade_charm(), base_state)
+    assert out.unit_status == BlockedStatus(
+        "Failed to set up the environment. Check `juju debug-log` for details."
+    )
+
+
+@patch("charm.Langpacks.import_gpg_key")
+def test_config_changed_no_secret(import_gpg_key_mock, ctx, base_state):
+    out = ctx.run(ctx.on.config_changed(), base_state)
+    assert out.unit_status == ActiveStatus("Signing disabled. Set the 'gpg-secret-id' to enable.")
+
+
+# needs to mock ops.SecretNotFoundError, ops.model.ModelError
+@patch("charm.Langpacks.import_gpg_key")
+def test_config_changed_secret_not_granted(import_gpg_key_mock, ctx, base_state):
+    config_secret = Secret(tracked_content={"key": "GPG_PRIVATE_KEY"})
+    state = State(leader=True, config={"gpg-secret-id": config_secret.id})
+    out = ctx.run(ctx.on.config_changed(), state)
+    assert out.unit_status == ActiveStatus("Secret not available. Check that access was granted.")
+
+
+@patch("charm.Langpacks.import_gpg_key")
+def test_config_changed_import_key_failure(mock, ctx, base_state):
+    mock.side_effect = CalledProcessError(1, "gpg")
+    config_secret = Secret(tracked_content={"key": "GPG_PRIVATE_KEY"})
+    state = State(leader=True, secrets=[config_secret], config={"gpg-secret-id": config_secret.id})
+    out = ctx.run(ctx.on.config_changed(), state)
+    assert out.unit_status == ActiveStatus(
+        "Failed to import the signing key. Check `juju debug-log` for details."
+    )
+
+
+@patch("charm.Langpacks.import_gpg_key")
+def test_config_changed(import_gpg_key_mock, ctx, base_state):
+    config_secret = Secret(tracked_content={"key": "GPG_PRIVATE_KEY"})
+    state = State(leader=True, secrets=[config_secret], config={"gpg-secret-id": config_secret.id})
+    out = ctx.run(ctx.on.config_changed(), state)
+    assert out.unit_status == ActiveStatus()
+    assert import_gpg_key_mock.called
+
+
+@patch("charm.Langpacks.update_checkout")
+def test_start_success(update_checkout_mock, ctx, base_state):
+    out = ctx.run(ctx.on.start(), base_state)
+    assert out.unit_status == ActiveStatus()
+    assert update_checkout_mock.called
+
+
+@patch("charm.Langpacks.update_checkout")
+@pytest.mark.parametrize(
+    "exception",
+    [CalledProcessError(1, "git"), GitCommandError(command="git pull", status=128)],
+)
+def test_start_failure(mock, exception, ctx, base_state):
+    mock.side_effect = exception
+    out = ctx.run(ctx.on.start(), base_state)
+    assert out.unit_status == BlockedStatus(
+        "Failed to start services. Check `juju debug-log` for details."
+    )
+
+
+@patch("charm.Langpacks.build_langpacks")
+def test_build_langpacks_success(build_langpacks_mock, ctx, base_state):
+    out = ctx.run(
+        ctx.on.action("build-langpacks", params={"release": "questing", "base": True}),
+        base_state,
+    )
+    assert ctx.action_logs == ["Building langpacks, it may take a while"]
+    assert out.unit_status == ActiveStatus()
+    assert build_langpacks_mock.called
+
+
+@patch("charm.Langpacks.build_langpacks")
+@pytest.mark.parametrize("exception", [IOError, RequestException, CalledProcessError(1, "build")])
+def test_build_langpacks_failure(mock, exception, ctx, base_state):
+    mock.side_effect = exception
+    out = ctx.run(
+        ctx.on.action("build-langpacks", params={"release": "questing", "base": True}),
+        base_state,
+    )
+    assert ctx.action_logs == [
+        "Building langpacks, it may take a while",
+        "Langpacks build failed",
+    ]
+    assert out.unit_status == ActiveStatus(
+        "Failed to build langpacks. Check `juju debug-log` for details."
+    )
+    # assert build_langpacks_mock.called
+
+
+@patch("charm.Langpacks.check_gpg_key")
+@patch("charm.Langpacks.upload_langpacks")
+def test_upload_langpacks_success(upload_langpacks_mock, check_gpg_key_mock, ctx, base_state):
+    check_gpg_key_mock.return_value = True
+    out = ctx.run(ctx.on.action("upload-langpacks"), base_state)
+    assert ctx.action_logs == ["Uploading langpacks, it may take a while"]
+    assert out.unit_status == ActiveStatus()
+    assert upload_langpacks_mock.called
+
+
+@patch("charm.Langpacks.check_gpg_key")
+def test_upload_langpacks_no_key(check_gpg_key_mock, ctx, base_state):
+    check_gpg_key_mock.return_value = False
+    out = ctx.run(ctx.on.action("upload-langpacks"), base_state)
+    assert ctx.action_logs == ["Can't upload langpacks without a signing key"]
+    assert out.unit_status == ActiveStatus(
+        "Upload disabled. Set and grant 'gpg-secret-id' to enable."
+    )
+
+
+@patch("charm.Langpacks.check_gpg_key")
+@patch("charm.Langpacks.upload_langpacks")
+def test_upload_langpacks_failure(upload_langpacks_mock, check_gpg_key_mock, ctx, base_state):
+    check_gpg_key_mock.return_value = True
+    upload_langpacks_mock.side_effect = CalledProcessError(1, "upload")
+    out = ctx.run(ctx.on.action("upload-langpacks"), base_state)
+    assert out.unit_status == ActiveStatus(
+        "Failed to upload langpacks. Check `juju debug-log` for details."
+    )
+
+
+@patch("charm.Langpacks.disable_crontab")
+def test_stop_sucess(disable_crontab_mock, ctx, base_state):
+    out = ctx.run(ctx.on.stop(), base_state)
+    assert out.unit_status == MaintenanceStatus("Removing crontab")
+    assert disable_crontab_mock.called
+
+
+@patch("charm.Langpacks.disable_crontab")
+def test_stop_failure(disable_crontab_mock, ctx, base_state):
+    disable_crontab_mock.side_effect = CalledProcessError(1, "crontab")
+    out = ctx.run(ctx.on.stop(), base_state)
+    assert out.unit_status == MaintenanceStatus("Removing crontab")
+    assert disable_crontab_mock.called
